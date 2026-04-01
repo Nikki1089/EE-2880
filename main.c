c41e7a04c9a206bbe016233e0bd49c61e1e72ef1
Nikki1089 <Nicole04@iastate.edu>
2026-04-01 13:42:31 -0500

    Update main.c

--- main.c
+++ main.c
@@ -29,12 +29,12 @@
 
 int main(void)
 {
-    button_init();
+    //button_init();
     timer_init(); // Must be called before lcd_init(), which uses timer functions
     lcd_init();
 
     // initialize the cyBot UART1 before trying to use it
-    uart_interrupt_init();
+    //uart_interrupt_init();
 
 adc_init();
 
@@ -54,18 +54,18 @@ adc_init();
 //    cyBot_uart_init_last_half();  // Complete the UART device configuration
 
     // Initialize the scan
-    cyBOT_init_Scan(0b0111);
+    //cyBOT_init_Scan(0b0111);
     // Remember servo calibration function and variables from Lab 3
     right_calibration_value = 290500;
     left_calibration_value = 1288000;
 
-    object_t objs[MAX_OBJS];
+   // object_t objs[MAX_OBJS];
 //    int i, smallest_i = 0;
 //    float smallest_width;
     size_t objc;
     int timeout, smallest_i;
     float distleft;
-    oi_t *bump = oi_alloc();
+   // oi_t *bump = oi_alloc();
     oi_init(bump);
 
     while (1)
@@ -82,61 +82,61 @@ adc_init();
 //        // uart_sendChar('\n');
 //        // uart_sendf("Received a '%c'\r\n", cmd);
 
-        if (command_flag)
-        {
-            char cmd = command_byte;
-            command_flag = 0;
+//         if (command_flag)
+//         {
+//             char cmd = command_byte;
+//             command_flag = 0;
 
-            switch (cmd)
-            {
-            case CMD_SERVO_CAL:
-                cyBOT_SERVO_cal();
-                break;
-            case CMD_SWEEP_IR:
-                objc = sweep_ir(objs);
-                if (objc <= 0)
-                    break;
-                smallest_i = get_smallest_i(objc, objs);
-                distleft = objs[smallest_i].dist * 6;
-                uart_printf(
-                        "navigating towards object %d (angle: %d, distance: %f)\n",
-                        smallest_i, MIDPOINT(objs[smallest_i]), distleft);
-                if (MIDPOINT(objs[smallest_i]) > 90)
-                {
-                    uart_printf("turning %d degrees counterclockwise\n",
-                    MIDPOINT(objs[smallest_i]) - 90);
-                    turn_ccw(bump, MIDPOINT(objs[smallest_i]) - 90, SPEED / 4);
-                }
-                else if (MIDPOINT(objs[smallest_i]) < 90)
-                {
-                    uart_printf("turning %d degrees clockwise\n",
-                                90 - MIDPOINT(objs[smallest_i]));
-                    turn_cw(bump, 90 - MIDPOINT(objs[smallest_i]), SPEED / 4);
-                }
-                for (timeout = 0; distleft > 10 && timeout < 10; ++timeout)
-                {
-                    distleft = move_forward(bump, distleft, SPEED);
-                    uart_printf("%f\n", distleft);
-                    if (bump->bumpLeft)
-                    {
-                        distleft += 25 - move_backward(bump, 25, SPEED, 1);
-                        turn_cw(bump, 90, SPEED / 4);
-                        move_forward(bump, 25, SPEED / 2);
-                        turn_ccw(bump, 90, SPEED / 4);
-                    }
-                    else if (bump->bumpRight)
-                    {
-                        distleft += 50 - move_backward(bump, 50, SPEED, 1);
-                        turn_ccw(bump, 90, SPEED / 4);
-                        move_fwd(bump, 100, SPEED / 2);
-                        turn_cw(bump, 90, SPEED / 4);
-                    }
-                }
-                break;
+//             switch (cmd)
+//             {
+//             case CMD_SERVO_CAL:
+//                 cyBOT_SERVO_cal();
+//                 break;
+//             case CMD_SWEEP_IR:
+//                 objc = sweep_ir(objs);
+//                 if (objc <= 0)
+//                     break;
+//                 smallest_i = get_smallest_i(objc, objs);
+//                 distleft = objs[smallest_i].dist * 6;
+//                 uart_printf(
+//                         "navigating towards object %d (angle: %d, distance: %f)\n",
+//                         smallest_i, MIDPOINT(objs[smallest_i]), distleft);
+//                 if (MIDPOINT(objs[smallest_i]) > 90)
+//                 {
+//                     uart_printf("turning %d degrees counterclockwise\n",
+//                     MIDPOINT(objs[smallest_i]) - 90);
+//                     turn_ccw(bump, MIDPOINT(objs[smallest_i]) - 90, SPEED / 4);
+//                 }
+//                 else if (MIDPOINT(objs[smallest_i]) < 90)
+//                 {
+//                     uart_printf("turning %d degrees clockwise\n",
+//                                 90 - MIDPOINT(objs[smallest_i]));
+//                     turn_cw(bump, 90 - MIDPOINT(objs[smallest_i]), SPEED / 4);
+//                 }
+//                 for (timeout = 0; distleft > 10 && timeout < 10; ++timeout)
+//                 {
+//                     distleft = move_forward(bump, distleft, SPEED);
+//                     uart_printf("%f\n", distleft);
+//                     if (bump->bumpLeft)
+//                     {
+//                         distleft += 25 - move_backward(bump, 25, SPEED, 1);
+//                         turn_cw(bump, 90, SPEED / 4);
+//                         move_forward(bump, 25, SPEED / 2);
+//                         turn_ccw(bump, 90, SPEED / 4);
+//                     }
+//                     else if (bump->bumpRight)
+//                     {
+//                         distleft += 50 - move_backward(bump, 50, SPEED, 1);
+//                         turn_ccw(bump, 90, SPEED / 4);
+//                         move_fwd(bump, 100, SPEED / 2);
+//                         turn_cw(bump, 90, SPEED / 4);
+//                     }
+//                 }
+//                 break;
 
-            default:
-                break;
-            }
-        }
-    }
-}
+//             default:
+//                 break;
+//             }
+//         }
+//     }
+// }
