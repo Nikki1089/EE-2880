72f92078e4cba4a795dff412389cf0dd84eb87f7
Nikki1089 <Nicole04@iastate.edu>
2026-04-01 15:37:00 -0500

    Update ping_template.h

--- ping_template.h
+++ ping_template.h
@@ -16,9 +16,34 @@
  */
 void ping_init (void);
 
-/**
- * @brief Trigger the ping sensor
- */
+void ping_trigger (void){
+    g_state = LOW;
+
+    // Disable timer and disable timer interrupt
+    TIMER3_CTL_R &= ~0x0100;
+    TIMER3_IMR_R &= ~0x0400;
+
+    // Disable alternate function (disconnect timer from port pin)
+    GPIO_PORTB_AFSEL_R &= ~0x08;
+
+    // Use PB3 as normal GPIO output to send trigger pulse
+    GPIO_PORTB_DIR_R |= 0x08;           // PB3 output
+    GPIO_PORTB_DATA_R &= ~0x08;         // Start low
+    timer_waitMicros(2);
+    GPIO_PORTB_DATA_R |= 0x08;          // High pulse
+    timer_waitMicros(5);
+    GPIO_PORTB_DATA_R &= ~0x08;         // End pulse
+    GPIO_PORTB_DIR_R &= ~0x08;          // Back to input
+
+    // Clear an interrupt that may have been erroneously triggered
+    TIMER3_ICR_R |= 0x0400;
+
+    // Re-enable alternate function, timer interrupt, and timer
+    GPIO_PORTB_AFSEL_R |= 0x08;
+    TIMER3_IMR_R |= 0x0400;
+    TIMER3_CTL_R |= 0x0100;
+}
+
 void ping_trigger (void);
 
 /**
